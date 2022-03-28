5f24f084232467900860a18e55d2a4db7be1c0e6
Kwon Kyoung min <lanos5019@ajou.ac.kr>
2022-03-28 17:02:11 +0900

    3.28commit

--- src/server.js
+++ src/server.js
@@ -1,33 +1,20 @@
 import express from "express";
-
+import morgan from "morgan";
 const PORT  = 4000;
 const app = express();
+const logger = morgan("dev");
+//morgan 흠.. logger임 그냥 뭐 사용된 메소드나 아니면 경로같은거 보여주는
 
-const logger = (req, res, next) => {
-    console.log(`goin to ${req.url}`);
-    //자 여기서 req는 get method를 타고온 겁니다.
-    next();
-};
-const handleHome = (req, res) => {
-    return res.send("home");
-};
 
-app.get("/", handleHome);
-
+const handleHome = (req,res) =>{
+    return res.send("hello");
+}
+const handleLogin = (req,res) => {
+    return res.send("login");
+}
 app.use(logger);
-//누군가가 "/"페이지 즉 root 페이지로 get request를 보낸다면!!!  콜백 함수 시행
-
-
-
-
-
-
-
-
-
-
-
-
+app.get("/", handleHome);
+app.get("/login", handleLogin);
 
 const handleServer = () => console.log(`server start to ${PORT}`);
 /**
@@ -37,4 +24,6 @@ const handleServer = () => console.log(`server start to ${PORT}`);
 */
 app.listen(PORT, handleServer);
 /* app.listen 은 말그대로 request를 listen
- 하는거임 ${PORT}포트로 누가 들어간 행동을 listen*/
+ 하는거임 ${PORT}포트로 누가 들어간 행동을 listen
+ 4000번 창문을 열어서 누가 들어오나 확인하는 거지
+*/
