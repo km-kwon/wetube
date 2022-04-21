a5ffb0c64ea732af49369f5fb1b4f551aa1eb7b6
Kwon Kyoung min <lanos5019@ajou.ac.kr>
2022-04-21 20:33:53 +0900

    4.21 loginsystem

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -8,11 +8,17 @@ export const getJoin = (req,res) =>{
 }
 export const postJoin = async (req,res) =>{
     const { username, email, password, password2, name, location } = req.body;
+    const pageTitle = "Join";
+    if(password !== password2){
+        return res.status(400).render("Join",{
+            pageTitle,
+            error: "enter same password"
+        });
+    }
     const exists = await User.exists({$or: 
         [{username: username}, {email: email}]
         //배열로 하나하나 조건 설정
     });
-    const pageTitle = "Join";
     if(exists){
         return res.status(400).render("Join",{
             //오류라고 http로 보내줘야 하기 때문에 status(400)이라고 하면 
@@ -21,12 +27,7 @@ export const postJoin = async (req,res) =>{
             error: "username or email is already exists"
         });
     }
-    if(password !== password2){
-        return res.status(400).render("Join",{
-            pageTitle,
-            error: "enter same password"
-        });
-    }else{
+    try{
         await User.create({
             username,
             email,
@@ -34,11 +35,15 @@ export const postJoin = async (req,res) =>{
             name,
             location
         });
-        console.log(req.body);
         //비밀번호 암호화를 위해 bcrypt란걸 사용할껀데 
         //해커가 해싱된 password를 가지고 할수 있는 공격이 있다.
         //그게 바로 rainbow table
-        return res.redirect("/");
+        return res.redirect("/login");
+    }catch(err){
+        return res.status(400).redirect("/join", {
+            pageTitle: getJoin,
+            error: err
+        });
     }
 };
 export const getLogin = (req,res) => {
@@ -61,9 +66,12 @@ export const postLogin = async (req,res) =>{
             pageTitle: "Login",
             error: "wrong password"
         });
-    }else{
-        return res.redirect("/");
     }
+
+    req.session.loggedIn = true;
+    req.session.user = user;
+    //req.session 객체에 user랑 loggedIn 플래그를 추가
+    return res.redirect("/");
     /*
     const checkpassword = await bycrpt.hash(password, 5);
     왜 이건 안돼 ㅅㅄㅄㅂㅂㅄㅄㅂ
--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 import express from "express";
 import morgan from "morgan";
+import session from "express-session";
 import routeRouter from "./routers/routeRouter.js";
 import user from "./routers/userRouter.js";
 import video from "./routers/videoRouter.js";
+import { localsMiddleware } from "./middlewares.js";
 //export default 했으니 하나의 변수로써 가지고오면서 정의 
 
 
@@ -16,6 +18,14 @@ app.use(express.urlencoded({extended: true}));
 //express 는 form을 인식 못함 그래서 이 문장 써주면 부야! 작동되쥬
 //req.body라는 형식으로 전달함 특정 url에서 쏴주는 거임(js 표현식 같은것)
 
+app.use(
+    session({
+        secret: "hello",
+        resave: true,
+        saveUninitialized: true
+    })
+);
+app.use(localsMiddleware);
 app.use("/", routeRouter);
 app.use("/videos", video);
 app.use("/users", user);
